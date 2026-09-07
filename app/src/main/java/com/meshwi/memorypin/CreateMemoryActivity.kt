1dc6c5d3de2ef59a49ef88aea06a30ab280cd86d
Meshwi-Creates <meshwi.creates@email.com>
2026-09-07 19:58:35 +0530

    Add photo picker

--- app/src/main/java/com/meshwi/memorypin/CreateMemoryActivity.kt
+++ app/src/main/java/com/meshwi/memorypin/CreateMemoryActivity.kt
@@ -1,16 +1,51 @@
 package com.meshwi.memorypin
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CreateMemoryActivity : AppCompatActivity() {
+
+    private val selectedPhotos = ArrayList<Uri>()
+
+    private val photoPicker =
+        registerForActivityResult(
+            ActivityResultContracts.GetMultipleContents()
+        ) { uris ->
+
+            if (uris.size in 1..3) {
+
+                selectedPhotos.clear()
+                selectedPhotos.addAll(uris)
+
+                Toast.makeText(
+                    this,
+                    "${uris.size} photo(s) selected",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else if (uris.isNotEmpty()) {
+
+                Toast.makeText(
+                    this,
+                    "Please select maximum 3 photos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_create_memory)
 
+        val btnAddPhotos = findViewById<Button>(R.id.btnAddPhotos)
+
+        btnAddPhotos.setOnClickListener {
+            photoPicker.launch("image/*")
+        }
     }
 }
